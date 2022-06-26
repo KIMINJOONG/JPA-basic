e51e8b96c7c74b3b02cbc035e22cd6f4bd4f1b9a
KIMINJOONG <ijboym216@gmail.com>
2022-06-27 08:58:33 +0900

    1. 요구사항 분석과 기본 매핑

--- src/main/java/jpabasic/ex1hellojpa/Member.java
+++ src/main/java/jpabasic/ex1hellojpa/Member.java
@@ -6,32 +6,32 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.util.Date;
 
-@Entity
-@Getter
-@Setter
-public class Member {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
-
-    @Column(name = "name")
-    private String username;
-
-    private Integer age;
-
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-
-    @Lob
-    private String description;
-
-    public Member() {}
-
-}
+//@Entity
+//@Getter
+//@Setter
+//public class Member {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private String id;
+//
+//    @Column(name = "name")
+//    private String username;
+//
+//    private Integer age;
+//
+//    @Enumerated(EnumType.STRING)
+//    private RoleType roleType;
+//
+//    @Temporal(TemporalType.TIMESTAMP)
+//    private Date createdDate;
+//
+//    @Temporal(TemporalType.TIMESTAMP)
+//    private Date lastModifiedDate;
+//
+//    @Lob
+//    private String description;
+//
+//    public Member() {}
+//
+//}
